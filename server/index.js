b7f4564c9ce9daee3ac81133651ac67a00548728
Simon Lincoln <simon.lincoln@glassboxdigital.com>
2024-04-11 10:19:12 +0100

    all

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
-const express = require("express"),
+const express = require('express'),
   PORT = 5000,
-  app = express();
+  app = express()
 
-app.get("/api/v1", (req, res) => {
-  res.send("hello !!!!");
-});
+app.get('/api/v1', (req, res) => {
+  const d = new Date()
+  let h = ('00' + d.getUTCHours()).slice(-2)
+  let m = ('00' + d.getUTCMinutes()).slice(-2)
+  let s = ('00' + d.getUTCSeconds()).slice(-2)
+  res.send(`${h}:${m}:${s}`)
+})
 
-app.listen(PORT, () => console.log(`start listening on port : ${PORT}`));
+app.listen(PORT, () => console.log(`start listening on port : ${PORT}`))
